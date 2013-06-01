713047f0c2f5aa483ef2301c8c179dfbd1be2c85
Jim Fleming <Jim@Jims-MacBook-Air.local>
2013-05-31 20:24:25 -0700

    Updating splash page with new design

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,20 @@
 (function() {
   mixpanel.track('pageview');
 
+  var $modal = $('.modal');
+  $('.play').on('click', function() {
+    mixpanel.track('play');
+    $modal.show().animate({ opacity: 1 }, 200, 'ease-in-out');
+  });
+
+  $modal.on('click', function() {
+    $modal.animate({ opacity: 0 }, 200, 'ease-in-out', function() {
+      $modal.hide();
+    });
+  });
+
   $('button.submit').on('click', _.throttle(function() {
-    var $signup = $('input.email');
+    var $email = $('.email');
     var email = $email.val();
 
     if (email.indexOf('@') === -1) {
@@ -13,7 +25,7 @@
     $email.val('');
     $email.attr('disabled', true);
 
-    $('.input').hide();
+    $('.form').hide();
     $('.thanks').show();
 
     mixpanel.identify(email);
