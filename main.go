7368c2ed6e229f2ac3d282659409a0924dab037b
jimfleming <jimmy.fleming@gmail.com>
2013-09-14 14:12:37 -0700

    Fixing server logging

--- main.go
+++ main.go
@@ -3,13 +3,22 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
 )
 
 func main() {
-	http.Handle("/", http.FileServer(http.Dir(".")))
-	err := http.ListenAndServe(":8000", nil)
-	if err != nil {
-		panic(err)
-	}
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt)
+
+	go func() {
+		http.Handle("/", http.FileServer(http.Dir(".")))
+		err := http.ListenAndServe(":8000", nil)
+		if err != nil {
+			panic(err)
+		}
+	}()
+
 	fmt.Println("Listening on :8000")
+	<-sig
 }
